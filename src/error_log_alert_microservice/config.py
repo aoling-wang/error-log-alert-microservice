7714aa7edcfa5a10595eee89f1e4e08e903ad81f
Kevin Wang <aoling.wang@gmail.com>
2026-09-03 18:51:14 -0400

    Adjust config.py to utilize dotenv

--- src/error_log_alert_microservice/config.py
+++ src/error_log_alert_microservice/config.py
@@ -1,6 +1,8 @@
 import os
 from dataclasses import dataclass
+from dotenv import load_dotenv
 
+load_dotenv()
 
 @dataclass(frozen=True)
 class Settings:
@@ -9,8 +11,8 @@ class Settings:
 
 
 def load_settings() -> Settings:
-    log_file = os.getenv("LOG_FILE")
-    webhook_url = os.getenv("WEBHOOK_URL")
+    log_file = os.getenv("LOG_FILE", default="sample_logs/application.log")
+    webhook_url = os.getenv("WEBHOOK_URL", default="https://discord.com/api/webhooks/1545199188638179418/4sH_fD_RpBC0BZPOB_Z6zovxTKCMu2Ul9N4-XFIN7v1UHgFwAGpH7NcSbiNz0CqgDr0h")
 
     if not log_file:
         raise ValueError("LOG_FILE environment variable is required.")
